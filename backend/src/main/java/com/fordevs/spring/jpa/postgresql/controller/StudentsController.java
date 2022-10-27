f5685941785c7149be9204453db155945b550269
fungicoder <fungicoder@gmail.com>
2022-10-27 16:54:39 -0600

    Backend with two entities, student and department.

--- backend/src/main/java/com/fordevs/spring/jpa/postgresql/controller/StudentsController.java
+++ backend/src/main/java/com/fordevs/spring/jpa/postgresql/controller/StudentsController.java
@@ -1,26 +1,15 @@
 package com.fordevs.spring.jpa.postgresql.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
-import com.fordevs.spring.jpa.postgresql.model.Department;
 import com.fordevs.spring.jpa.postgresql.model.Student;
-import com.fordevs.spring.jpa.postgresql.repository.DepartmentRepository;
 import com.fordevs.spring.jpa.postgresql.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 
 @CrossOrigin(origins = "*", allowedHeaders = "*")
@@ -37,13 +26,13 @@ public class StudentsController {
     @GetMapping("/users")
     public ResponseEntity<List<Student>> getAllUsers(@RequestParam(required = false) String fullName) {
         try {
-            List<Student> students = new ArrayList<Student>();
+            List<Student> students = new ArrayList<>();
 
             if (fullName == null)
                 students.addAll(studentRepository.findAll());
 
             else
-            students.addAll(studentRepository.findByFullNameContaining(fullName));
+                students.addAll(studentRepository.findByFullNameContaining(fullName));
 
             if (students.isEmpty()) {
                 return new ResponseEntity<>(HttpStatus.NO_CONTENT);
